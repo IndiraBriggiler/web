5a4e7223fa262f4e006a62da4898d71574f9b1bf
Indira Briggiler <indira@eudaimonia.com.ar>
2019-01-21 17:26:42 -0300

    mail fixed, images added

--- indira-briggiler/mail/contact_me.php
+++ indira-briggiler/mail/contact_me.php
@@ -14,10 +14,10 @@ $email_address = $_POST['email'];
 $message = $_POST['message'];
 	
 // Create the email and send the message
-$to = 'ibriggiler@gmail.com'; 
+$to = 'contact@indirabriggiler.com'; 
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: ibriggiler@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: contact@indirabriggiler.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
